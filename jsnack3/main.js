2417e12467ff0ce47635eb2a120609615c7468e9
Edoardo Lai <edoardolai96@gmail.com>
2022-01-27 20:23:48 +0100

    Fix click button

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -8,7 +8,7 @@ let inizia = document.getElementById("bottone");
 inizia.addEventListener("click" ,
 function(){
     
-    for (let i=0; i <= 10; i++){
+    for (let i=1; i <= 10; i++){
     
         somma += i;
     
@@ -18,6 +18,7 @@ function(){
     
     alert("Il risultato finale è: " + somma);
 
+    somma = 0;
 }
 
 
